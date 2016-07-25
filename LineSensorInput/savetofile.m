c54565bd4c320dc779dbeda72821dfc6ac77850b
Sandeep Thiruvalluvan <sandeep.thiru1996@gmail.com>
2016-07-25 19:19:21 +0530

    Update savetofile.m

--- LineSensorInput/savetofile.m
+++ LineSensorInput/savetofile.m
@@ -2,14 +2,11 @@
 s = serial('COM5','BaudRate',38400);
 set(s,'Terminator','CR');                        %set(s,'Terminator','CR');
 fopen(s);
-f = fopen('MyNewData.txt','a+');       %a+ r/w append data
+f = fopen('MyNewData.txt','a+');                 %a+ r/w append data
 
 while 1
-a = fscanf(s);             %a=fscanf(s,'%d',14)
-%a
-%end
-
-%f = fopen('MyData.txt','a+');       %a+ r/w append data
+a = fscanf(s);                                   %a=fscanf(s,'%d',14)
+%f = fopen('MyData.txt','a+');                   %a+ r/w append data
 fprintf(f,'%s\n',a);
 end
 
